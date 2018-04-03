afc1cca2c26b6a7dc70696e265653c7d6c649bfa
IWSR <lsh934960645@163.com>
2018-04-03 13:35:33 +0800

    拆除gulp与nodemon启动命令

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,36 @@ let tsp = ts.createProject('tsconfig.json'); //使用tsconfig.json文件配置ts
 let exec = require('child_process').exec;
 
 let child;
+
 //目录常量
 const PATHS = {
   scripts: ['./src/**/*.ts'],
   output: './dist',
 };
+
 //编译ts文件
 gulp.task('build-ts', function () {
   return gulp.src(PATHS.scripts)
     .pipe(tsp())
     .pipe(gulp.dest(PATHS.output));
 });
+
 //监视ts文件变化
-gulp.task('watch-ts', ['build-ts', 'restart'], function () {
-  gulp.watch(PATHS.scripts, ['build-ts', 'restart']);
+gulp.task('watch-ts', ['build-ts'], function () {
+  gulp.watch(PATHS.scripts, ['build-ts']);
 });
+
 //自动重启服务器
-gulp.task('restart', ['build-ts'], function (error) {
-  console.log('restart node');
-  child = exec('nodemon ./dist/index.js', (error, stdout, stderr) => {
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-    if (error !== null) {
-      console.log(`exec error: ${error}`);
-    }
-  });
-});
+// gulp.task('restart', ['build-ts'], function (error) {
+//   console.log('restart node');
+//   child = exec('nodemon ./dist/index.js', (error, stdout, stderr) => {
+//     console.log(`stdout: ${stdout}`);
+//     console.log(`stderr: ${stderr}`);
+//     if (error !== null) {
+//       console.log(`exec error: ${error}`);
+//     }
+//   });
+// });
+
 //开发任务
-gulp.task('dev', ['build-ts', 'restart', 'watch-ts']);
-// gulp.task('dev', ['build-ts', 'watch-ts']);
+gulp.task('dev', ['build-ts', 'watch-ts']);
