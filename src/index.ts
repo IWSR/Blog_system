2dd0b4ad7933aa3fdbb6ea26ff7d6a04c8d218d6
IWSR <lsh934960645@163.com>
2018-04-17 10:18:45 +0800

    不知道改了啥

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import Koa from 'koa';
+import * as Koa from 'koa';
 
 const app = new Koa();
 
 app.use(async ctx => {
   ctx.body = 'hello world';
 });
+let a = 3;
+console.log(a);
 console.log('123');
-app.listen(3000);
+console.log('1234');
+app.listen(3001);
