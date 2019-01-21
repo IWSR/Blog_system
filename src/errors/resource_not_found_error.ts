361597d28837f613261380bd2a0b1f350f62c9af
IWSR <lsh934960645@163.com>
2019-01-21 15:31:11 +0800

    refine: 删除多余注释

--- src/errors/resource_not_found_error.ts
+++ src/errors/resource_not_found_error.ts
@@ -1,11 +1,10 @@
-// const HTTPBaseError = require('./http_base_error');
 import HTTPBaseError from "./http_base_error";
 
 const ERROR_CODE = 404000;
 
 class ResourceNotFoundError extends HTTPBaseError {
 
-  constructor(methodName: string, resourceId: string, httpMsg: number) {
+  constructor(methodName: string, resourceId: string, httpMsg: string) {
     super(404, httpMsg, ERROR_CODE, `${methodName} not found, id: ${resourceId}`);
   }
 }
