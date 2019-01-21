a80f7f2e6d2a20195b2b75f40b30177cd2fb96db
Asuka <lsh934960645@163.com>
2019-01-21 15:36:58 +0800

    Revert "Feature/login"

--- src/routes/api/login.ts
+++ src/routes/api/login.ts
@@ -3,11 +3,19 @@ import * as Router from "koa-router";
 const router = new Router();
 
 router.get("/", (ctx, next) => {
+  // ctx.response.set("Set-Cookie", "userName=hahaha");
   const { username } = ctx.query;
+  // const session = ctx.session;
+  ctx.session = {};
+  // ctx.session.user = username;
+  // ctx.session.wahaha = "wahaha";
   ctx.body = "login";
 });
 
 router.get("/hello", (ctx, next) => {
+  // if (ctx.cookies.get("userName")) {
+  //   ctx.body = "<h1>肥宅快乐饼</h1>";
+  // }
   const userName = ctx.session.user;
   ctx.body = `hello ${userName}`;
 });
